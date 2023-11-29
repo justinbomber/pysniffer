99a7fe55ba2039a830833006bf4626f77d15e594
justinbomber <46494090+justinbomber@users.noreply.github.com>
2023-11-29 17:56:04 +0800

    更新startsniffer.sh

--- python_sniffer/startsniffer.sh
+++ python_sniffer/startsniffer.sh
@@ -11,10 +11,9 @@ trap cleanup INT
 
 echo "start sniffer.py and dbwriter.py..."
 
-./dbwriter -t 3 -d postgresql://postgres:admin@140.110.7.17:5433/postgres &
-sleep 9
-
 ./capture -i any -p 15000 &
+sleep 2
+./dbwriter -t 3 -d postgresql://postgres:admin@140.110.7.17:5433/postgres &
 
 wait
 
